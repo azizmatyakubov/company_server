fd3cc5ce868d4382773022ddde9f20b3e9ee4cbe
AZIZ <a.matyaqubov1471@gmail.com>
2022-11-29 00:46:52 +0100

    create endpoint for getUserById

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,25 +1,29 @@
+import mongoose from 'mongoose';
 import Users from '../models/UsersModel.js';
+import createHttpError from 'http-errors';
 
 
-export const getAllUsers = async (req, res) => {
+
+export const getAllUsers = async (req, res, next) => {
     try {
         const users = await Users.find();
-        res.status(200).json(users);
+        res.send(users);
     } catch (error) {
         res.status(500).json({ error: error.message });
     }
 }
 
-export const getUserById = async (req, res) => {
+export const getUserById = async (req, res, next) => {
     try {
         const { id } = req.params;
+        if (!mongoose.Types.ObjectId.isValid(id)) return next(createHttpError(400, `The id ${id} is not valid`));
+    
         const user = await Users.findById(id);
-        if (user) {
-            return res.status(200).json(user);
-        }
-        res.status(404).json({ message: 'User not found' });
+        if(!user) return next(createHttpError(404, `User with id ${id} not found`));
+        
+        res.send(user);
     } catch (error) {
-        res.status(500).json({ error: error.message });
+        next(error);
     }
 }
 
