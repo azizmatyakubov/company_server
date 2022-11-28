57ea75e912f8d8c1babf3f98b504809832bd75c6
AZIZ <a.matyaqubov1471@gmail.com>
2022-11-29 00:32:00 +0100

    add validator to login

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,7 +1,22 @@
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
+import User from '../models/userModel.js';
+import authValidator from '../validators/authValidator.js';
 
 
-export const login = async (req, res) => {}
 
-export const register = async (req, res) => {}
+export const login = async (req, res) => {
+    try {
+        // Validate request body
+        const { error } = authValidator.login.body.validate(req.body);
+        if (error) {
+            return res.status(400).json({ error: error.message });
+        }
+
+
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+
+}
+
