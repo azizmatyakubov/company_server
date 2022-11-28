02b609e40c124e3f8f2c24a5e8f35a828ae34eab
AZIZ <a.matyaqubov1471@gmail.com>
2022-11-28 23:46:15 +0100

    connect DB

---
+++ src/config/db.js
@@ -0,0 +1,12 @@
+import mongoose from 'mongoose'
+
+
+export const connectDB = () => {
+    try {
+        mongoose.connect(process.env.MONGO_URI)
+        console.log('Connected to MongoDB')
+    } catch (error) {
+        console.log(error)
+    }
+};
+
--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import express from 'express';
 import dotenv from 'dotenv';
 
+import { connectDB } from './config/db.js';
+
+
 
 const app = express();
 
 // Load env vars
 dotenv.config({ path: './config/config.env' });
+
+// Connect to database
+connectDB();
+
 
 const PORT = process.env.PORT || 5000;
 
