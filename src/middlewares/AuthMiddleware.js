77508723ad924fb457c8aabe73c9a8ee16d31f36
AZIZ <a.matyaqubov1471@gmail.com>
2022-11-30 16:07:03 +0100

    fix access token bug

--- src/middlewares/AuthMiddleware.js
+++ src/middlewares/AuthMiddleware.js
@@ -5,10 +5,10 @@ export default (req, res, next) => {
     const token = authHeader && authHeader.split(' ')[1]
   
     if (token == null) return res.status(401).send('Unauthorized')
-  
-    jwt.verify(token, ACCESS_TOKEN_SECRET, (err, decoded) => {
+    
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
         if (err) return res.status(403).send('Forbidden')
-            req.user = decoded
-            next()
+        req.user = decoded
+        next()
     })
 }
