30ff54ad4f04290ada393cddba81f66b6b7b5f05
AZIZ <a.matyaqubov1471@gmail.com>
2022-12-04 00:17:16 +0100

    clean code

--- src/routes/departmentsRoute.js
+++ src/routes/departmentsRoute.js
@@ -1,14 +1,9 @@
 import express from 'express';
-
 import { createNewDepartment, getAllDepartments, getDepartmentById, updateDepartment, deleteDepartment, getAllDepartmentByName  } from '../controllers/departmentsController.js';
-
 import AuthMiddleware from '../middlewares/AuthMiddleware.js';
-
 import { role } from '../middlewares/RoleMiddleware.js';
 
 const departmentsRouter = express.Router();
-
-
 
 departmentsRouter.post('/', AuthMiddleware, role(['admin']), createNewDepartment)
 departmentsRouter.get('/', AuthMiddleware, role(['admin', 'user']), getAllDepartments )
@@ -16,6 +11,5 @@ departmentsRouter.get('/search', AuthMiddleware, role(['admin', 'user']), getAll
 departmentsRouter.get('/:id', AuthMiddleware, role(['admin', 'user']), getDepartmentById )
 departmentsRouter.put('/:id', AuthMiddleware, role(['admin']), updateDepartment )
 departmentsRouter.delete('/:id', AuthMiddleware, role(['admin']), deleteDepartment )
-
 
 export default departmentsRouter;
